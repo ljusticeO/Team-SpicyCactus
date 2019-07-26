79b49521006598783a4eaa54c93ca58b68a520d9
crazywolf132 <crazywolf132@gmail.com>
2019-07-26 12:41:33 +1000

    Added test files

---
+++ src/test/java/com/team1/spicycactus/services/WebLayerTest.java
@@ -0,0 +1,39 @@
+package com.team1.spicycactus.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WebLayerTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void apiCreateDriver() {
+        
+    }
+
+    @org.junit.jupiter.api.Test
+    void apiGetAllDrivers() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void apiGetDriver() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void apiGetCar() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void apiGetElectricCars() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void mockAPICars() {
+    }
+}
