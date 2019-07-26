0e3c292395d474aadf9591c63eda38cd2d96bb59
crazywolf132 <crazywolf132@gmail.com>
2019-07-26 11:44:46 +1000

    Added checks for driver in db

---
+++ src/main/java/com/team1/spicycactus/business/Drivers.java
@@ -0,0 +1,20 @@
+package com.team1.spicycactus.business;
+
+import com.team1.spicycactus.bean.Driver;
+import com.team1.spicycactus.dao.DriverRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Drivers {
+
+    @Autowired
+    DriverRepo driverRepo;
+
+    public boolean driverExists(int id){
+        return driverRepo.findById(id).isPresent();
+    }
+    public boolean driverExists(Driver driver){
+        return driverRepo.findById(driver.getDriver_id()).isPresent();
+    }
+
+    
+}
