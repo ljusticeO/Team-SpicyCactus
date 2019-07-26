8b2ce768088fe999be8eb609f7ea98d41e8c3979
Cam <camkaley@hotmail.com>
2019-07-26 11:24:21 +1000

    Web Layer Started

--- src/main/java/com/team1/spicycactus/services/WebLayer.java
+++ src/main/java/com/team1/spicycactus/services/WebLayer.java
@@ -1,4 +1,20 @@
 package com.team1.spicycactus.services;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/drivers")
 public class WebLayer {
+
+    @GetMapping
+
+
+//    http://localhost:8080/v1/drivers POST - Create Driver
+//    http://localhost:8080/v1/drivers GET - Get drivers
+//    http://localhost:8080/v1/drivers/{driverId} GET - Get Driver by ID
+//    http://localhost:8080/v1/drivers/selectcar/{driverId} GET - Get Car from driver id
+//    http://localhost:8080/v1/drivers/criteria/automaticElectric GET - Get cars that are automatic
+
 }
